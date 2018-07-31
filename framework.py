a35b3c25fd6a76bc854ee19e4be4db7317f4f828
Qinyuan Ye <qinyuan@ink-ron.usc.edu>
2018-07-31 15:02:21 -0700

    add batch size to saved file name

--- framework.py
+++ framework.py
@@ -367,8 +367,8 @@ class Framework(object):
 
         if not os.path.exists(FLAGS.test_result_dir):
             os.mkdir(FLAGS.test_result_dir)
-        np.save(os.path.join(FLAGS.test_result_dir, FLAGS.model_name + '_' + str(FLAGS.drop_prob) + '_' + str(FLAGS.learning_rate) + '_x.npy'), save_x)
-        np.save(os.path.join(FLAGS.test_result_dir, FLAGS.model_name + '_' + str(FLAGS.drop_prob) + '_' + str(FLAGS.learning_rate) + '_y.npy'), save_y)
+        np.save(os.path.join(FLAGS.test_result_dir, FLAGS.model_name + '_' + str(FLAGS.drop_prob) + '_' + str(FLAGS.learning_rate) + '_' + str(FLAGS.batch_size) + '_x.npy'), save_x)
+        np.save(os.path.join(FLAGS.test_result_dir, FLAGS.model_name + '_' + str(FLAGS.drop_prob) + '_' + str(FLAGS.learning_rate) + '_' + str(FLAGS.batch_size) + '_y.npy'), save_y)
         print('best epoch:', best_epoch)
         print('best f1 epoch:', best_f1_epoch)
         print('P, R, F1:', precision, ',', recall, ',', best_f1)
